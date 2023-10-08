4c721ffdcaf50061c67ce354bb2c466076372a06
Elder <eldercnw@gmail.com>
2023-10-07 21:25:54 -0300

    Adicionar arquivos de projeto.

---
+++ jurosApi/Controllers/JurosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using taxas.Models.Response;
+
+namespace taxas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JurosApiController : ControllerBase
+    {
+
+        [HttpGet("juros")]
+        public JurosResponse Get()
+        {
+            return new JurosResponse()
+            {
+                Juros = 0.01
+            };
+        }
+    }
+}
